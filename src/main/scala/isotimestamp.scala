c7e6fdf74485287974df16cbcbfdd8893c8c58a3
Daniel James <dwhjames@gmail.com>
2014-06-12 06:14:19 -0400

    extract timestamp impl

---
+++ src/main/scala/isotimestamp.scala
@@ -0,0 +1,12 @@
+package awseb
+
+import java.text.SimpleDateFormat
+import java.util.{ Date, TimeZone }
+
+object ISO8601 {
+  def timestamp(): String = {
+    val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
+    df.setTimeZone(TimeZone.getTimeZone("UTC"))
+    df.format(new Date());
+  }
+}
--- src/main/scala/upload.scala
+++ src/main/scala/upload.scala
@@ -6,16 +6,11 @@ import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.elasticbeanstalk.model.S3Location
 
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.{ Date, TimeZone }
 
 object S3FileUploader {
 
   def upload(s3: AmazonS3, bucketName: String, file: File): S3Location = {
-    val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
-    df.setTimeZone(TimeZone.getTimeZone("UTC"))
-    val nowAsISO8601 = df.format(new Date());
-    val key = s"${nowAsISO8601}-${Integer.toString(util.Random.nextInt(), 36)}-${file.getName}"
+    val key = s"${ISO8601.timestamp()}-${Integer.toString(util.Random.nextInt(), 36)}-${file.getName}"
 
     val location = new S3Location(bucketName, key)
 
